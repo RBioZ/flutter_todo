94b09ecd9fdaec48eb77756820357bc64c6719ac
RBioZ <jonatha_rihan@hotmail.com>
2021-10-14 20:53:30 -0300

    :sparkles: Aula #02

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,34 @@ class _HomeState extends State<Home> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Lista de Tarefas"),
+        backgroundColor: Colors.blueAccent,
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.fromLTRB(17.0, 1.0, 7.0, 1.0),
+            child: Row(
+              children: [
+                Expanded(
+                  child: TextField(
+                    decoration: InputDecoration(
+                        labelText: "Nova Tarefa",
+                        labelStyle: TextStyle(color: Colors.blueAccent)),
+                  ),
+                ),
+                ElevatedButton(
+                  onPressed: () {},
+                  child: Text('ADD'),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
   }
 }
